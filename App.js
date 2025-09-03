78e1fde5f837cffbd35af362c5f79a984336db72
Leonardo Souza <leonardosm.act@gmail.com>
2025-09-03 19:09:59 -0300

    Preparando a interface do projeto.

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, Platform, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Button } from './src/components/Button';
+import { styles } from './App.styles'
+import { currencies } from './src/constants/currencies'
+import { Input } from './src/components/input';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+    <KeyboardAvoidingView
+      style={styles.contanier}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    >
+
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.content}>
+          <StatusBar style="light" />
+
+          <View style={styles.header}>
+            <Text style={styles.title}>Conversor de Moedas</Text>
+            <Text style={styles.subTitle}>
+              Converta valores entre diferentes moedas
+            </Text>
+          </View>
+          <View style={styles.card}>
+            <Text styles={styles.label}>De:</Text>
+
+            <View style={styles.currencyGrid}>
+              {currencies.map(currency => (
+                <Button variant='primary'
+                  key={currency.code}
+                  currency={currency}
+                >
+                </Button>
+              ))}
+
+            </View>
+              <Input/>
+            
+          </View>
+
+
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
+
+
+
+  );
+};
